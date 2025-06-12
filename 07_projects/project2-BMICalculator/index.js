a949bf80b6d563eaf5ad35183149b4c228b7dc49
nirmakar24 <karnirmalya2003@gmail.com>
2025-06-12 22:15:29 +0530

    project2 done

---
+++ 07_projects/project2-BMICalculator/index.js
@@ -0,0 +1,39 @@
+function calculateBMI() {
+  const form = document.getElementById('bmiForm');
+
+  form.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    const weight = parseFloat(document.getElementById('weightInput').value);
+    const height = parseFloat(document.getElementById('heightInput').value);
+    const result = document.getElementById('result');
+
+    if (!height || height <= 0) {
+      result.innerHTML = `⚠️ Please enter a valid height.`;
+      return;
+    }
+
+    if (!weight || weight <= 0) {
+      result.innerHTML = `⚠️ Please enter a valid weight.`;
+      return;
+    }
+
+    const BMI = (weight / ((height * height) / 10000)).toFixed(2);
+    let message = '';
+
+    if (BMI < 18.5) {
+      message = 'You are <strong>Underweight</strong>.';
+    } else if (BMI < 24.9) {
+      message = 'You have a <strong>Normal weight</strong>.';
+    } else if (BMI < 29.9) {
+      message = 'You are <strong>Overweight</strong>.';
+    } else {
+      message = 'You are <strong>Obese</strong>.';
+    }
+
+    result.innerHTML = `Your BMI is <strong>${BMI}</strong><br>${message}`;
+  });
+}
+
+// Call the function once DOM is loaded
+window.addEventListener('DOMContentLoaded', calculateBMI);
